8b5d23ce2614fc38e51bd1d55e4a2430fb68a619
albertzhang24 <skyline24a@gmail.com>
2018-06-20 14:15:21 -0700

    Add files via upload

---
+++ testing_sox.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+num=0
+
+while read arg1 arg2; do
+  printf "Num: $num\n"
+	printf "Start: ${arg1} End : ${arg2}\n"
+  dif=$(python -c "print($arg2-$arg1)")
+  printf "Difference is: $dif\n"
+  ~/downloads/sox-14.4.2/sox 16k_1.wav 16k_1_$num.wav trim $arg1 $dif
+  num=$((num+1))
+# Pipe data into the while loop
+done < /Users/albertzhang/desktop/text_files/txt/formatted_sound_intervals.txt
